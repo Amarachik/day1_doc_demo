da4b27ed0700e04730c01efa54aeb634c0cd66e4
Amarachi Kalu-Onuma <akaluonuma@gmail.com>
2019-07-01 11:57:12 -0700

    Remove Elements in

--- day1_doc/MyPlayground.playground/Contents.swift
+++ day1_doc/MyPlayground.playground/Contents.swift
@@ -2,4 +2,7 @@ import UIKit
 
 var str = "Hello, playground"
 var arr = [1, 3, 5, 7]
+arr.remove(at:1) //removes the integer at index 1
+print (arr) //prints [1,5,7]
+
 // remove and print the 2nd element
